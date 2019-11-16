655588b381453df0e191fc16dd12050fd4488848
Michael Waldo <mbwcogic@yahoo.com>
2019-11-16 23:55:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,4 +14,16 @@ def save
  def self.all
    @@all
 end
+
+def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
